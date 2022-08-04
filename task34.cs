2d61d0577d9ae1a2b8a63045d6d91c9affe18052
unknown <toghrul@code.edu.az>
2022-08-04 17:05:55 +0400

    .

---
+++ task34.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TASK_HOME1
+{
+    class Program
+    {
+        static void Min (string[] args)
+        {
+            int[] nums = { 2, 5, 7, 8, 9 };
+            Console.WriteLine(Min(nums));
+            return ();
+
+        }
+        static int Min(int nums);
+
+
+        {
+    int num = nums[0];
+    for (int i = 1; i<nums.Length; i++)
+			
+
+			
+
+
+			
+
+
